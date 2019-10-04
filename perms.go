00b58d171948b87a1cffc38ebfdb5e2386fce4ea
Jacob Wiltse <jwiltse@control-tec.com>
2019-10-04 12:10:24 -0400

    Fix multi-value context issue

--- perms.go
+++ perms.go
@@ -17,7 +17,7 @@ func (m *CasbinMiddleware) Casbin(fn router.HandlerFunc) router.HandlerFunc {
 		if err != nil {
 			return
 		}
-		if m.Enforce(ctx.Msg.Author.ID, ctx.Msg.GuildID, ctx.Route.Name, "execute") || guild.OwnerID == ctx.Msg.Author.ID {
+		if res := m.Enforce(ctx.Msg.Author.ID, ctx.Msg.GuildID, ctx.Route.Name, "execute"); res || guild.OwnerID == ctx.Msg.Author.ID {
 			fn(ctx)
 		} else {
 			_, _ = ctx.ReplyEmbed("You do not have permission to use this command.")
