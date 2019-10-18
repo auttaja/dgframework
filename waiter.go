864df0b584e6b28f45ee62781fccc582d1f152b9
Jacob Wiltse <jwiltse@control-tec.com>
2019-10-18 11:27:10 -0400

    Hide waiter variables that don't needt to be public

--- waiter.go
+++ waiter.go
@@ -9,24 +9,24 @@ import (
 type CheckFunc func(interface{}) bool
 
 type Waiter struct {
-	Checker    CheckFunc
+	checker    CheckFunc
 	Response   chan interface{}
-	EventType  reflect.Type
+	eventType  reflect.Type
 	removeFunc func()
 }
 
 func WaitFor(s *discordgo.Session, event interface{}, checkFunc CheckFunc) *Waiter {
 	w := new(Waiter)
-	w.EventType = reflect.TypeOf(event)
+	w.eventType = reflect.TypeOf(event)
 	w.Response = make(chan interface{}, 1)
-	w.Checker = checkFunc
+	w.checker = checkFunc
 	w.removeFunc = s.AddHandler(w.waitCallback)
 	return w
 }
 
 func (w *Waiter) waitCallback(s *discordgo.Session, event interface{}) {
-	if reflect.TypeOf(event).Elem() == w.EventType {
-		if w.Checker(event) {
+	if reflect.TypeOf(event).Elem() == w.eventType {
+		if w.checker(event) {
 			w.removeFunc()
 			w.Response <- event
 		}
